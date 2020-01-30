555036098845fd8b3c1854b67cdc5e8a71134932
Neji <naji.trabelsi@alterway.fr>
2020-01-30 15:21:14 +0100

    add sequilize

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const mysql = require("mysql");
+const Sequelize = require('sequelize');
 
+const seq = new Sequelize('database', 'username', 'password');
 const app = express(); 
 
-const PORT = process.env.PORT || 4000 ; 
+const PORT = process.env.PORT || 5000 ; 
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "dbuser",
-  password: "s3kreee7"
+  host: "172.18.0.1",
+  user: "sampleuser",
+  database:"sampledb",
+  password: "samplepassword",
+  port: 3308
 });
 
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!"); 
+  connection.query("SELECT * FROM test", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result); 
+  });
+}); 
+ 
 app.get('/', (req, res)=>{
     const obj = { a: 1 };
     res.setHeader("Content-Type", "application/json");
